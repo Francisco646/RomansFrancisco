854fb77a8d8c7a0b7c263c33b2dd96efbc877422
Francisco646 <101455034+Francisco646@users.noreply.github.com>
2024-05-06 17:34:13 +0200

    Update romans.js

--- romans.js
+++ romans.js
@@ -11,14 +11,6 @@ function init() {
   var convertButton = document.querySelector(".convert-button");
   var outputArea = document.querySelector(".convert-output");
   var inputArea = document.querySelector("input[type='text']");
-
-  function assertEquals(actual, expected, message) {
-    if (actual === expected) {
-        console.log(message + ": Passed");
-    } else {
-        console.error(message + ": Failed. Expected " + expected + ", but got " + actual);
-    }
-  }
   
   modeCheckbox.addEventListener("change", function(e) {
     header.innerHTML = getModeTitle(e.target.checked);
