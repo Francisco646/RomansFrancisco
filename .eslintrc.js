a47e7ee21f3b0191b23f33b0533d6dba0319ce1f
Francisco646 <101455034+Francisco646@users.noreply.github.com>
2024-05-06 19:42:43 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,26 +10,17 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
-    "no-cond-assign": [
-            "error",
-            "always"
-        ],
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "no-unused-vars": "off",
-        "semi": [
-            "error",
-            "always"
-        ]
+    "no-cond-assign": ["error", "always"],
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error","single"],
+    "no-unused-vars": "off",
+    "semi": ["error","always"],
+    "no-extra-semi": "error",
+    "no-mixed-spaces-and-tabs": "error",
+    "no-undef": "error",
+    "no-unreachable": "error",
+    "no-use-before-define": "error",
+    "comma-dangle": ["error", "always-multiline"]
   },
 };
